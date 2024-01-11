455a932b7aada1d3c95bd3dfad838da0c463b086
lastofyears21 <lastofyears21@gmail.com>
2024-01-11 17:56:14 +0900

    24/01/11 정벼리 리퍼지토리 등록

---
+++ src/main/java/com/HealthCare/HealthyLife_Backend/repository/SeasonRankingRepository.java
@@ -0,0 +1,7 @@
+package com.HealthCare.HealthyLife_Backend.repository;
+
+import com.HealthCare.HealthyLife_Backend.entity.SeasonRanking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SeasonRankingRepository extends JpaRepository<SeasonRanking, Long> {
+}
