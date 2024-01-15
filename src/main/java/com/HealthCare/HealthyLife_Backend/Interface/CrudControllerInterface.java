213b3f2f2f65713e4c78fdca27cc662b7ccca823
lastofyears21 <lastofyears21@gmail.com>
2024-01-15 20:58:17 +0900

    24/01/15 정벼리 빅데이터 수집 로직 진행중

--- src/main/java/com/HealthCare/HealthyLife_Backend/Interface/CrudControllerInterface.java
+++ src/main/java/com/HealthCare/HealthyLife_Backend/Interface/CrudControllerInterface.java
@@ -1,11 +1,13 @@
-package com.HealthCare.HealthyLife_Backend.controller;
+package com.HealthCare.HealthyLife_Backend.Interface;
 
 import org.springframework.http.ResponseEntity;
 import java.util.List;
 public interface CrudControllerInterface<T, ID> {
+
+
     ResponseEntity<List<T>> findAll();
     ResponseEntity<T> findById(ID id);
-    ResponseEntity<T> insert(T dto);
+    ResponseEntity<?> insert();
     ResponseEntity<T> update(ID id, T dto);
     ResponseEntity<Void> delete(ID id);
 
---
+++ src/main/java/com/HealthCare/HealthyLife_Backend/controller/MedicineController.java
@@ -0,0 +1,62 @@
+package com.HealthCare.HealthyLife_Backend.controller;
+
+import com.HealthCare.HealthyLife_Backend.Interface.CrudControllerInterface;
+import com.HealthCare.HealthyLife_Backend.dto.MedicineDto;
+
+
+import com.HealthCare.HealthyLife_Backend.service.MedicineService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/medicines")
+public class MedicineController implements CrudControllerInterface<MedicineDto, Long> {
+
+    private final MedicineService medicineService;
+
+
+    public MedicineController(MedicineService medicineService) {
+        this.medicineService = medicineService;
+
+    }
+
+
+    @Override
+    public ResponseEntity<?> insert() {
+        try {
+            Map<String, String> funtionalities = medicineService.getFunctionalities();
+            Map<String, String> medicineList = medicineService.getMedicineList();
+            return ResponseEntity.ok("건강기능식품 insert");
+        } catch (Exception e) {
+            return ResponseEntity.notFound().build();
+        }
+
+    }
+
+
+
+    @Override
+    public ResponseEntity<List<MedicineDto>> findAll() {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<MedicineDto> findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Void> delete(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<MedicineDto> update(Long id, MedicineDto medicineDto) {
+        return null;
+    }
+
+}
