1874bbeb5c353975982bf2a50fc09155281f63f6
moonjiye <mjy6088@gmail.com>
2024-01-18 00:52:18 +0900

    24/01/18 뭉 캘린더수정

--- src/main/java/com/HealthCare/HealthyLife_Backend/dto/CalendarDto.java
+++ src/main/java/com/HealthCare/HealthyLife_Backend/dto/CalendarDto.java
@@ -1,5 +1,7 @@
 package com.HealthCare.HealthyLife_Backend.dto;
 
+import com.HealthCare.HealthyLife_Backend.entity.Calendar;
+import com.HealthCare.HealthyLife_Backend.entity.SeasonRanking;
 import lombok.*;
 import java.time.LocalDate;
 
@@ -8,10 +10,21 @@ import java.time.LocalDate;
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CalendarDto {
-    private Long calendarId;
-    private String calendarTitle; //제목
-    private String calendarDetail; // 내용
-    private LocalDate calendarWriteDate; //작성일자
-    private Integer points;
+    private Long id;
+    private String title; //제목
+    private String detail; // 내용
+    private LocalDate writeDate; //작성일자
+    private Integer points; //포인트
+
+    public Calendar toCalendarEntity() {
+        return Calendar.builder()
+                .id(this.getId())
+                .title(this.getTitle())
+                .detail(this.getDetail())
+                .writeDate(this.getWriteDate())
+                .points(this.getPoints())
+                .build();
+    }
 }
