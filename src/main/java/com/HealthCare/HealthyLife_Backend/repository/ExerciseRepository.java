0138142d0869b8041dd1dda6b70c2f6140ce31e4
PANG2OPPA <khbkhb85098509@gmail.com>
2024-01-19 15:11:49 +0900

    2024.01.19 김현빈 운동 데이터 불러오기, 번역 기능 완료 (API 요청횟수 제한으로 변경 필수)

---
+++ src/main/java/com/HealthCare/HealthyLife_Backend/repository/ExerciseRepository.java
@@ -0,0 +1,8 @@
+package com.HealthCare.HealthyLife_Backend.repository;
+
+import com.HealthCare.HealthyLife_Backend.entity.Exercise;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
+}
+
