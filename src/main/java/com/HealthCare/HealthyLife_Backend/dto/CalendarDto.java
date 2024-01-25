03105f22c7905739024a28245a647c3ebc44b7d5
moonjiye <mjy6088@gmail.com>
2024-01-25 20:48:50 +0900

    24/01/25 문지예 캘린더내용추가

--- src/main/java/com/HealthCare/HealthyLife_Backend/dto/CalendarDto.java
+++ src/main/java/com/HealthCare/HealthyLife_Backend/dto/CalendarDto.java
@@ -1,16 +1,48 @@
 package com.HealthCare.HealthyLife_Backend.dto;
 
+import com.HealthCare.HealthyLife_Backend.entity.Calendar;
 import lombok.*;
 import java.time.LocalDate;
 
+@Builder
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class CalendarDto {
+
     private Long calendarId;
     private String calendarTitle; //제목
     private String calendarDetail; // 내용
     private LocalDate calendarWriteDate; //작성일자
+
+    private Long id;
+    private Integer month;
+    private String title; //제목
+    private String detail; // 내용
+    private LocalDate writeDate; //작성일자
+
+    private String morning; // 아침
+    private String lunch; // 점심
+    private String dinner; // 저녁
+    private String workout; // 운동
+
+    private Integer carbohydrate; // 탄수화물
+    private Integer protein; // 단백질
+    private Integer province; // 지방
+    private Integer calorie; //칼로리
+    private Integer proportion;// 비율
+
+    private Integer points; //포인트
+
+    public Calendar toCalendarEntity() {
+        return Calendar.builder()
+                .id(this.getId())
+                .title(this.getTitle())
+                .detail(this.getDetail())
+                .writeDate(this.getWriteDate())
+                .points(this.getPoints())
+                .build();
+    }
 }
--- src/main/java/com/HealthCare/HealthyLife_Backend/entity/Calendar.java
+++ src/main/java/com/HealthCare/HealthyLife_Backend/entity/Calendar.java
@@ -1,20 +1,22 @@
 package com.HealthCare.HealthyLife_Backend.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import com.HealthCare.HealthyLife_Backend.dto.CalendarDto;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
+
 
 @Entity
 @Table(name = "calendar_tb") // 실제 데이터베이스 테이블 이름에 맞게 지정해야 합니다.
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 @ToString
+@Builder
 public class Calendar {
     @Id
     @Column(name = "calendarId")
@@ -29,4 +31,52 @@ public class Calendar {
 
     @Column(name = "calendar_write_date",nullable = false)
     private LocalDate diaryWriteDate; //작성일자
+
+    private int month;
+    @Column(nullable = false)
+    private String title; // 제목
+    @Column(length = 1000)
+    private String detail; // 내용
+    @Column(name = "write_date", nullable = false)
+    private LocalDate writeDate; // 작성일자
+
+    private String morning; // 아침
+    private String lunch; // 점심
+    private String dinner; // 저녁
+    private String workout; // 운동
+
+    private Integer carbohydrate; // 탄수화물
+    private Integer protein; // 단백질
+    private Integer province; // 지방
+    private Integer calorie; // 칼로리
+    private Integer proportion; // 비율
+
+    private Integer points; // 포인트
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "body_id")
+    private Body body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "exercise_id")
+    private Exercise exercise;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "food_id")
+    private Food food;
+
+    @OneToMany(mappedBy = "calendar", cascade = CascadeType.ALL)
+    private List<SeasonRanking> seasonRankings;
+
+    public CalendarDto toCalendarDto() {
+        return CalendarDto.builder()
+                .id(this.getId())
+                .month(this.getMonth())
+                .title(this.getTitle())
+                .detail(this.getDetail())
+                .writeDate(this.getWriteDate())
+                .points(this.getPoints())
+                .build();
+    }
 }
